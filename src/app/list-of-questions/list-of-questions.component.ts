4231886032365919423a562f0b0e507c8f891619
Ekaterina Popova <eo.popova@yandex.ru>
2020-03-19 00:54:40 +0300

    sorting questions by like

--- src/app/list-of-questions/list-of-questions.component.ts
+++ src/app/list-of-questions/list-of-questions.component.ts
@@ -9,23 +9,36 @@ import { MatInput } from '@angular/material/input';
   styleUrls: ['./list-of-questions.component.css']
 })
 export class ListOfQuestionsComponent implements OnInit {
-  public questionsAndLikes = {};
-// public question2 = [];
-// numberOfLikes = [];
-like = false;
+  listOfItems = [];
+  // public questionsAndLikes = new Map();
+
   constructor() { }
 addToList(evnt) {
-    this.questionsAndLikes[evnt] = 0;
+    // this.questionsAndLikes.set('question', evnt);
+    // this.questionsAndLikes.set('likes', 0);
+    this.listOfItems.push({
+      question: evnt,
+      likes: 0,
+      likeBoolean: false
+    });
+    console.log(this.listOfItems);
 }
   ngOnInit(): void {
   }
-  getLike(condition: boolean, question: any) {
+  getLike(condition: boolean, id: number) {
     if (!condition) {
-      this.questionsAndLikes[question] += 1;
-      this.like = !this.like;
+      this.listOfItems[id].likes += 1;
+      this.listOfItems[id].likeBoolean = !this.listOfItems[id].likeBoolean;
     } else {
-      this.questionsAndLikes[question] -= 1;
-      this.like = !this.like;
+      this.listOfItems[id].likes -= 1;
+      this.listOfItems[id].likeBoolean = !this.listOfItems[id].likeBoolean;
     }
+    console.log(this.listOfItems, this.listOfItems[id].likeBoolean);
+    // a = this.listOfItems[id - 1].likes;
+    // b = this.listOfItems[id].likes;
+    // tslint:disable-next-line:only-arrow-functions
+    this.listOfItems = this.listOfItems.sort(function(a, b) {
+        return b.likes - a.likes;
+    });
   }
 }
