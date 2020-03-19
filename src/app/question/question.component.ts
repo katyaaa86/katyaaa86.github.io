9ed0f885176f9b90328ce6ed87271e9966eba429
Ekaterina Popova <eo.popova@yandex.ru>
2020-03-20 00:53:16 +0300

    form validation

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,5 +1,5 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {FormControl} from '@angular/forms';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-question',
@@ -19,7 +19,7 @@ export class QuestionComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.question = new FormControl('');
+    this.question = new FormControl('', [Validators.required]);
   }
 
 }
